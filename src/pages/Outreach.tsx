1005e6cb68dd1bfb314e6797e473f1bb77929854
Joseph Alex <josephalex125@hotmail.com>
2026-09-01 11:32:48 -0400

    Comment out PlaceholderImage import in Outreach.tsx
    Commented out the import statement for PlaceholderImage.

--- src/pages/Outreach.tsx
+++ src/pages/Outreach.tsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { PlaceholderImage } from "../components/PlaceholderImage";
+// import { PlaceholderImage } from "../components/PlaceholderImage";
 
 export default function Outreach() {
   return (
